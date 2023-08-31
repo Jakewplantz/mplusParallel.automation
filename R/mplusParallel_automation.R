4dd6f49dccfe02dd4f07275ba2622296ec393c42
Jakewplantz <jakewplantz@gmail.com>
2023-08-31 12:02:23 -0400

    Initial Commit

---
+++ R/mplusParallel_automation.R
@@ -0,0 +1,377 @@
+#' Parallel automation of running Mplus models using R.
+#'
+#' This function provides a parallelized automation for Mplus when using R as the data generation method. When `data_gen`
+#' is specified, include any arguments from the function that need to be set in the global enviornment.
+#'
+#'
+#' @param k Number of replications desired.
+#' @param k.start Defaults to 1. Specifies the replication to start on.
+#'    Useful if the simulation stopped on a specific replication and resuming without loss of work.
+#' @param data_gen Can take either a single dataframe in the 'folder' or a data
+#'   generation function. When it is a data generation function any arguments
+#'   for data generation should be specified in this function.
+#' @param seed Seed defaults to 123 but can be any integer. This ensures every
+#'   replication's data is generated using a different seed but is reproducible.
+#' @param ncores Defaults to the number of cores on the machine - 1.
+#' @param useCores Logical. When TRUE, the mplus files will be adjusted to use
+#'   the number of cores on the machine. This can speed up simulation run times.
+#' @param cores_per_analysis Applies when `useCores` is TRUE. Default is `ncores/2`.
+#'   If you experience issues or crashes due to memory or core use, set this lower.
+#'   In testing the default will use most of a computer's CPU power but no break the simulation.
+#' @param Par_plan Plan for parallel processing. Defaults to 'cluster'. Can take any argument from the 'future' package
+#' @param rec Logical. Indicates if the files are in subdirectories.
+#' @param folder Defaults to the pathway of your R script. Path to the root folder
+#'   of where your mplus files are located.
+#' @param run Logical. Defaults to T. When T the Mplus models will be run. When F models will not be run and the output files will be read in only.
+#' @param results Indicates which results to collect. Supports summaries, parameters,
+#'   and modindicies or any named list argument output by mplus automation. When using summaries, parameters, or modindicies
+#'   mnore specific output is available.
+#' @param params_ext When `results` is 'parameters', specifies parameter type for
+#'   extraction. Can take any type but defaults to 'unstandardized'.
+#'   If you do not desire unstandardized parameters read in an output file to
+#'   determine the name of the parameters of interest and use this as the named argument.
+#' @param specific_sums Extracts specific output when `results` is 'summaries'.
+#' @param specific_params Extracts specific parameters when `results` is 'parameters'.
+#' @param item Extracts specific items when `results` is 'parameters'.
+#' @param modV1s Used for specific output when `results` is 'mod_indicies'.
+#' @param ops Operator for `modV1s`, e.g., 'BY' for factor loadings.
+#' @param modV2s Second variable for `modV1s`.
+#' @param custom_auto User-defined function for running and reading in models.
+#' Only functions that return single dataframes each run are currently supported.
+#' @param retry Logical. Defaults to TRUE. Retries with a new seed if chi is not
+#'   returned by the model.
+#' @param max_retry Defaults to 5. Specifies how many times a new seed should
+#'   be attempted.
+#'
+#' @return Function returns a dataframe of all the desired parameters for each replication.
+
+
+
+
+mplusParallel_automation <- function(k, k.start = 1,  data_gen = NA, seed = 123, ncores = 'default', run = T,
+                                     useCores = T, cores_per_analysis = 'default', Par_plan = 'cluster',
+                                     rec = F, results = c(), params_ext = c('unstandardized'),
+                                     specific_sums = NULL,
+                                     specific_params = NULL, item = NULL,
+                                     modV1s = NULL,ops = NULL, modV2s = NULL,
+                                     custom_auto = NULL, retry = T, max_retry = 5,
+                                     folder = (dirname(rstudioapi::getActiveDocumentContext()$path))){
+  ### --- Parallel Processing --- ###
+  k.start = k.start
+  k.range = k
+  if (ncores == 'default'){
+    ncores = detectCores()
+  } else {
+    ncores = ncores
+  }
+  Par_plan <- as.vector(Par_plan)
+  plan(Par_plan, workers = ncores-1)
+  set.seed(seed)
+  all_seeds <- sample.int(100000, 100000, replace = TRUE)
+  seeds <- all_seeds[1:k.range]
+
+  ### --- Internal Functions ----##
+
+  # --- Function to leverage cores in mplus analyses ---- #
+  # -----------------------------------------------------
+  # Description:
+  # This function detects the number of cores available
+  # and tells mplus to use this amount of cores.
+  # If cores_per_analysis is set to 'default' it will use ncores/2 for each analysis
+  # Otherwise, it will use the specified amount.
+  # -----------------------------------------------------
+  replace_line <- function(file_path) {
+    lines <- readLines(file_path)
+    # Check if cores_per_analysis is neither 'default' nor an integer
+    if(tolower(cores_per_analysis) != 'default' && (!is.numeric(cores_per_analysis) || cores_per_analysis != floor(cores_per_analysis))) {
+      stop("cores_per_analysis is not set to 'default' or an integer.")
+    }
+    # Throw an error if cores_per_analysis exceeds ncores
+    if(is.numeric(cores_per_analysis) && cores_per_analysis > ncores) {
+      stop("cores_per_analysis exceeds ncores available.")
+    }
+
+    # Determine replacement_line based on the conditions provided
+    if(tolower(cores_per_analysis) == 'default') {
+      replacement_line <- paste0("PROCESSORS=", ceiling(ncores/2), ";")
+    } else {
+      replacement_line <- paste0("PROCESSORS=", ceiling(cores_per_analysis), ";")
+    }
+
+    # Check if a line containing "PROCESSORS" exists
+    if(any(grepl("^\\s*PROCESSORS.*$", lines, ignore.case = TRUE))) {
+      # Replace the line containing "PROCESSORS"
+      lines <- gsub("^\\s*PROCESSORS.*$", replacement_line, lines)
+    } else {
+      # If no replacement was done
+      analysis_position <- which(grepl("analysis", lines, ignore.case = TRUE))
+      if (length(analysis_position) > 0) {
+        lines <- append(lines, replacement_line, after = analysis_position[1])
+      } else {
+        lines <- c(replacement_line, lines)
+      }
+    }
+
+    writeLines(lines, file_path)
+  }
+  # --- Function to rewrite data being used to function call ---- #
+  # -----------------------------------------------------
+  # Description:
+  # replaces the File is line in mplus files with exdat.csv
+  # -----------------------------------------------------
+  rewrite_data_in_inp_files <- function(file_path) {
+
+    # Read the file
+    file_content <- readLines(file_path, warn = FALSE)
+
+    # Combine all lines into a single string to handle multi-line patterns
+    full_text <- paste(file_content, collapse = "\n")
+
+    # Replace the string after "DATA:" and "FILE IS" until the semicolon with "exdat.csv"
+    modified_text <- gsub("(DATA:[[:space:]\n]*FILE IS[[:space:]]*).+?;", "\\1exdat.csv;", full_text)
+
+    # Split the modified text back into lines
+    modified_lines <- unlist(strsplit(modified_text, split = "\n"))
+
+    # Write the modified content back to the file
+    writeLines(modified_lines, file_path)
+  }
+  ### ---- Function to copy mplus models to processing folder --- ###
+
+  # -----------------------------------------------------
+  # Description:
+  # This function copies the contents of the root folder 'folder'
+  # into each process folder
+  # -----------------------------------------------------
+  copy_folder_contents <- function(source_folder, destination_folder) {
+    # List all files in the source folder
+    all_contents <- list.files(source_folder, full.names = TRUE)
+
+    # Filter out directories
+    files <- all_contents[!sapply(all_contents, function(path) { isTRUE(file.info(path)$isdir) })]
+
+    # Copy each file to the destination folder
+    sapply(files, function(file) {
+      file.copy(file, file.path(destination_folder, basename(file)), overwrite = TRUE)
+    })
+  }
+
+  ### ---- Function to create processing folders and data to use in parallel --- ###
+
+
+  # -----------------------------------------------------
+  # Description:
+  # This function is used to automate the folder creation for paralell procesing
+  # -----------------------------------------------------
+  autoFun <- function(k,seed) {
+
+    # Changing files to use exdat.csv
+    path_to_files <- folder
+    files <- list.files(path_to_files, pattern = "\\.inp$", full.names = TRUE)
+    lapply(files, rewrite_data_in_inp_files)
+
+
+    #--- Changing the cores used by each .inp file to make use of locally available maximums
+    #--- Path to the directory containing the .inp files
+    if(useCores == TRUE) {
+
+      lapply(files, replace_line)
+    }
+
+    ## Begin parallel processing
+    #### Error coding for failure of seed to produce results.
+    ### Errors are stored, then new seed is selected and it is ran again
+
+
+    pid <- as.numeric(Sys.getpid())
+
+    session_folder <- file.path(folder, paste0("session_", pid, "_k_", k))
+
+    if (!dir.exists(session_folder)) {
+      dir.create(session_folder)
+    }
+
+    copy_folder_contents(folder, session_folder)
+    subdirs <- list.dirs(folder, full.names = TRUE, recursive = rec)
+
+
+    if (grepl("\\bseed\\b|\\bseeds\\b", data_gen, ignore.case = TRUE)) {
+      warning("The text contains 'seed' or 'seeds', which may alter reproducibility.")
+    }
+
+    # Use tryCatch to detect errors
+    result <- tryCatch({
+      # Try to evaluate the expression
+      eval(parse(text = data_gen))
+    }, error = function(e) {
+      # If an error occurs, stop the function with a specific message
+      stop("Error during evaluation: ", e$message, '\nPlease ensure all objects used by the function are defined within it.')
+    })
+
+    # Check if the "data" object exists after the evaluation
+    if (!exists("data", inherits = FALSE)) {
+      stop("The function passed to data_gen did not result in an object named 'data'. Halting execution.")
+    }
+
+    rows_list <- list()
+
+    #-------------------------------------------------------#
+    # Analyze data using mplus automation - when custom auto is entered this section is replaced
+    #-------------------------------------------------------#
+    if (is.null(custom_auto)){
+      retries <- 0  # A counter to avoid infinite loops, consider a limit like 10 retries
+      max_retries <- max_retry
+      df_ready <- FALSE  # A flag to indicate when the data frame is ready
+      #-------------------------------------------------------#
+      # START
+      # The following function runs all models in the folders set above, then extracts the relevant stats
+      #-------------------------------------------------------#
+      while (!df_ready && retries < max_retries) {
+
+        filepath <- file.path(session_folder, "exdat.csv")
+
+        write.table(data, filepath, col.names = FALSE, row.names = FALSE, sep = ",")
+
+        if (run == T){
+          runModels(session_folder, showOutput = F, recursive = rec)
+        }
+        test_res <- assign(paste0("sums_", pid),readModels(session_folder, what = 'summaries', recursive = rec))
+        t <- test_res[[2]]
+
+
+        # Extract the chi value (assuming it is in models_result, adapt as needed)
+        chi_value <- t$ChiSqM_Value  # Change this line to accurately capture the chi value from your results
+
+        if (is.null(chi_value)) {
+          # Reset seed (adapt the way you want to reset it)
+          set.seed(k*10000)
+
+          retries <- retries + 1
+          next  # Skip the rest of the loop and retry
+        }
+
+        if (retries == max_retries) {
+          stop("Max retries reached. Could not get a non-null chi value.")
+        }
+        df_ready <- TRUE
+      }
+
+
+
+      if(results == 'summaries'){
+        models_result <- assign(paste0("sums_", pid),readModels(session_folder, what = results, recursive = rec))
+        res <- models_result[[2]]
+        res <- res %>%
+          mutate(Rep = k) %>%
+          select(Rep, everything())
+        new_row <- res
+        rows_list[[length(rows_list) + 1]] <- new_row
+
+      } else if (results == 'parameters'){
+        models_result <- assign(paste0("sums_", pid),readModels(session_folder, what = results, recursive = rec))
+        res <- models_result
+        new_data <- res[[2]][[params_ext]]
+
+        new_data <- new_data %>%
+          mutate(Rep = k, ParType = params_ext) %>%
+          select(Rep, everything())
+        new_row <- new_data
+        rows_list[[length(rows_list) + 1]] <- new_row
+
+      } else if (results == 'mod_indices'){
+        models_result <-  assign(paste0("sums_", pid),readModels(session_folder, recursive = rec))
+        res <- models_result[["mod_indices"]]
+        new_data <- res
+        new_data <- new_data %>%
+          mutate(Rep = k) %>%
+          select(Rep, everything())
+        new_row <- new_data
+        rows_list[[length(rows_list) + 1]] <- new_row
+
+      } else {
+        models_result <-  assign(paste0("sums_", pid),readModels(session_folder, recursive = rec))
+        res <- models_result[[results]]
+        new_data <- res
+        new_data <- new_data %>%
+          mutate(Rep = k) %>%
+          select(Rep, everything())
+        new_row <- new_data
+        rows_list[[length(rows_list) + 1]] <- new_row
+      }
+
+      df <- do.call(rbind,rows_list)
+      return(df)
+    } else {
+      eval(parse(text = custom_auto))
+
+    }
+  }
+
+  ### --- Function for parallel processing --- ####
+  options(future.rng.onMisuse= 'ignore')
+  df.all <- future_map2(1:k.range, seeds, autoFun)
+
+
+
+  ### --- Final Dataframe partioning --- ###
+  df.final <- as.data.frame(do.call(rbind, df.all))
+
+
+  if (!is.null(specific_sums)) {
+    if (results == 'summaries') {
+      # Ensure 'Rep' is always included
+      specific_sums <- unique(c("Rep", specific_sums))
+
+      # Subset df.final by the columns present in specific_sums
+      # Note: select() will automatically retain the "Rep" column
+      df.final <- df.final %>%
+        select(all_of(specific_sums))
+
+      # Check for missing columns after subsetting
+      missing_cols <- setdiff(specific_sums, colnames(df.final))
+
+      # Throw a warning if some columns are missing
+      # Warning for missing columns, only for 'summaries'
+      if (results == 'summaries' && length(missing_cols) > 0) {
+        warning(paste("The following columns were not found in df.final:",
+                      paste(missing_cols, collapse = ", "),
+                      '\n Make sure you are not requesting information from a different Results call'))
+      }
+    } else if (results == 'parameters') {
+      if (!is.null(specific_params)) {
+        df.fin <- df.final[df.final$paramHeader %in% toupper(specific_params),]
+        df.final = df.fin
+      }
+
+      if (!is.null(item)) {
+        df. <- df.fin[df.fin$param %in% toupper(item),]
+        df.final <- df.
+      }
+
+
+    } else if (results == 'mod_indices') {
+
+      if (!is.null(modV1s)) {
+        df.fin <- df.final[df.final$modV1 %in% toupper(modV1s)]
+        df.final <- df.fin
+      }
+
+      if (!is.null(ops)) {
+        df. <- df.fin[df.fin$op %in% toupper(ops)]
+        df.final <- df.
+      }
+
+      if (!is.null(modV2s)) {
+        df.fi <- df.[df.$modV2 %in% toupper(modV2)]
+        df.final = df.fi
+      }
+
+    } else{
+      df.final = df.final
+    }
+
+  }
+  ### --- Removing of subdirectories created --- ###
+
+  return(df.final)
+}
---
+++ R/removeParFolders.R
@@ -0,0 +1,31 @@
+#' Remove Parallel Processing Folders from mplusParallel_automation
+#'
+#' This function is used to delete all parallel processing folders (with names 
+#' containing the word "session") that were created by the `mplusParallel_automation` 
+#' function.
+#'
+#' @param folder The root directory to search for parallel processing folders.
+#'   Defaults to the directory of the currently active RStudio document.
+#'
+#' @return Invisible NULL. The function is called for its side effect of 
+#'   deleting folders.
+#' @export
+#' @examples
+#' # Assuming you have parallel processing folders in your current RStudio 
+#' # document's directory
+#' removeParFolders()
+#'
+#' @seealso 
+#' \code{\link{mplusParallel_automation}} for the function that creates 
+#' these folders.
+
+
+
+
+removeParFolders <- function(folder =(dirname(rstudioapi::getActiveDocumentContext()$path))){
+  subdirs <- list.dirs(folder, full.names = TRUE, recursive = FALSE)
+  for (subdir in subdirs) {
+    if (grepl("session", basename(subdir))) {
+      unlink(subdir, recursive = TRUE)
+    }
+  }}
